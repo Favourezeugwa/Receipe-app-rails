4a3361ac135f39183a2092e5e1bd8ff14fce8cfe
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-11 19:24:04 +0530

    Error fixing

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 end
+=======
+class ApplicationMailer < ActionMailer::Base
+  default from: "from@example.com"
+  layout "mailer"
+end
+>>>>>>> Stashed changes
