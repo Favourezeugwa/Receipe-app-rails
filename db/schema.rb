0cb055da1466adb8bb82e57779ca2346dec35b44
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-11 20:07:06 +0530

    Add recipe model and migration with association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_141919) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_11_143416) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_141919) do
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "preparation_time"
+    t.string "cooking_time"
+    t.text "description"
+    t.boolean "public"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -29,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_141919) do
   end
 
   add_foreign_key "inventories", "users"
+  add_foreign_key "recipes", "users"
 end
