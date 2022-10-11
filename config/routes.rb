71089a9d8c271298395466bccc38fd59b13bb201
Favourezeugwa <favourezeugwa@gmail.com>
2022-10-11 13:18:44 +0100

    Modify root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "foods#index"
+  root "users#index"
   resources :foods, only: [:index]
   resources :receipes, only: [:index, :show]
   resources :inventories, only: [:index, :show]
