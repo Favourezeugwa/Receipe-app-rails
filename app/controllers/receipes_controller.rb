16906ecdba5dd88652a9dc8fee14facef9a04f70
rich <adukyerer@gmail.com>
2022-10-12 09:03:37 +0000

    Add recipe controller details

--- app/controllers/receipes_controller.rb
+++ app/controllers/receipes_controller.rb
@@ -1,5 +1,9 @@
 class ReceipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.all
+  end
 
-  def show; end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 end
