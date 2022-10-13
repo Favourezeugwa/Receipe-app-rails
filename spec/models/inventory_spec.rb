04859fa8865c473158f41b4cbd073c75b9314a53
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-13 19:23:28 +0530

    Add missing test

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Recipe, type: :model do
-  describe 'Tests for Recipe model validation ' do
+RSpec.describe Inventory, type: :model do
+  describe 'Tests for Inventory model validation ' do
     @user = User.new(name: 'Rito', email: 'abcd@gmail.com', password: '123456')
-    subject do
-      Recipe.new(user: @user, name: 'Hello', description: 'This is my first recipe description',
-                 preparation_time: '10:00min', cooking_time: '60min', public: true)
-    end
+    subject { Inventory.new(user: @user, name: 'Hello') }
     before { subject.save }
 
     it 'name should be present' do
@@ -24,16 +21,6 @@ RSpec.describe Recipe, type: :model do
              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
              took a galley of type and scrambled it to make a type specimen book.
               It has survived not only five centuries"
-      expect(subject).to_not be_valid
-    end
-
-    it 'preparation time should not be blank' do
-      subject.preparation_time = ''
-      expect(subject).to_not be_valid
-    end
-
-    it 'cooking time should not be blank' do
-      subject.cooking_time = ''
       expect(subject).to_not be_valid
     end
   end
