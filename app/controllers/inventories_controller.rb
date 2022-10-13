bc6dc713adae5dd7788ad6019019865bca96fba0
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-13 19:41:33 +0530

    Add inventory CRUD expect update, actions and routes

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,7 +1,30 @@
 class InventoriesController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @inventories = Inventory.all
+  end
 
-  def show; end
+  def show
+    @inventory = Inventory.find(params[:id])
+  end
+
+  def new
+    @inventory = Inventory.new
+  end
+
+  def create
+    @inventory = Inventory.new(inventory_params)
+    @inventory.user_id = current_user.id
+
+    return render :new, alert: 'Failed to add an Inventory. Error!!' unless @inventory.save
+
+    redirect_to @inventory, notice: 'Inventory added successfully.'
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root "public_recipes#index"
   resources :foods, only: [:index]
   resources :recipes, only: [:index, :show , :new, :create, :edit, :update, :destroy]
-  resources :inventories, only: [:index, :show]
+  resources :inventories, only: [:index, :show, :new, :create, :destroy]
   resources :shopping_lists, only: [:index]
   resources :public_recipes, only: [:index]
   resources :recipe_foods, only: [:new, :create, :edit, :update, :destroy]
